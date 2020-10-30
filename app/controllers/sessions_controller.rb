f5debc77909f2cd7e775ab2aa06d606d2cd59dad
florianempinet <fempinet@gmail.com>
2020-10-30 18:40:15 +0100

    Version Final

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # cherche s'il existe un utilisateur en base avec l’e-mail
     user = User.find_by(email: params[:email])
 
+    # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe
     if user && user.authenticate(params[:password])
       log_in(user)
       if params[:remember_me]
@@ -21,9 +23,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    user = User.find_by(email: params[:email])
-    session.delete(:user_id)
-    log_out(user)
+    log_out(current_user)
     redirect_to root_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,54 +1,35 @@
 module SessionsHelper
-    def log_in(user)
-        session[:user_id] = user.id 
-    end
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-    def current_user
-        if session[:user_id]
-            current_user = User.find_by(id: session[:user_id])
-          elsif cookies[:user_id]
-            user = User.find_by(id: cookies[:user_id])
-            if user && BCrypt::Password.new(user.remember_digest).is_password?(cookies[:remember_token])
-              log_in user
-              current_user = user
-            end
-          end
-      end
+  def remember(user)
+    # ici je vais créer un remember_token qui est une suite aléatoire de caractères
+    remember_token = SecureRandom.urlsafe_base64
 
-      def remember(user)
-        # ici je vais créer un remember_token qui est une suite aléatoire de caractères
-        remember_token = SecureRandom.urlsafe_base64
-      
-        # j'ai mon token, je vais stocker son digest en base de données :    
-        user.remember(remember_token)
-      
-        #  maintenant, je vais créer les cookies : un cookie qui va conserver l'user_id, et un autre qui va enregistrer le remember_token
-        cookies.permanent[:user_id] = user.id
-        cookies.permanent[:remember_token] = remember_token
-      
-      end
+    # j'ai mon token, je vais stocker son digest en base de données :
+    user.remember(remember_token)
 
-      def remember_temp(user)
-        remember_token = SecureRandom.urlsafe_base64  
-        user.remember(remember_token)
-        cookies.signed[:user_id] = user.id
-        cookies.signed[:remember_token] = remember_token
-      end
+    #  maintenant, je vais créer les cookies : un cookie qui va conserver l'user_id, et un autre qui va enregistrer le remember_token
+    cookies.permanent[:user_id] = user.id
+    cookies.permanent[:remember_token] = remember_token
+  end
 
-      def forget(user)
+  def remember_temp(user)
+    remember_token = SecureRandom.urlsafe_base64
+    user.remember(remember_token)
+    cookies.signed[:user_id] = user.id
+    cookies.signed[:remember_token] = remember_token
+  end
 
-        # on remet le remember_digest à nil puisqu'il ne nous servira plus :
-        user.update(remember_digest: nil)
-      
-        # on efface les cookies dans le navigateur de l'utilisateur
-        cookies.delete(:user_id)
-        cookies.delete(:remember_token)
-      
-      end
+  def forget(user)
+    user.update(remember_digest: nil)
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
-      def log_out(user)
-        user = User.find_by(id: cookies[:user_id])
-        session.delete(:user_id)
-        forget(user)
-      end
+  def log_out(user)
+    session.delete(:user_id)
+    forget(user)
+  end
 end
